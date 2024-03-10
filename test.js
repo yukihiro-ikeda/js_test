26a681dbfcf1b955e7a298fcff96aaeaf4ddc634
yukihiro ikeda <headlessrecord@gmail.com>
2024-03-11 01:54:00 +0900

    test

--- test.js
+++ test.js
@@ -2,7 +2,25 @@
 // color_list.forEach(function(color) {
 //     print(color)
 // })
+array1 = [...Array(10)].map((_,i)=>i)
+sakujo = Math.floor(Math.random() * 10)
+// splice (配列から削除)
+tmp1 = array1.splice(sakujo,1)
 
-function(){
-    console.log('hey')
+// act = []
+// tmp = []
+// target = []
+
+// for(i = 0;i <= 3;i++){
+//     act.push(i)
+// }
+console.log(array1,sakujo)
+
+for(i = 10,j= 0;i >= 0,j <= 10;i--,j++){
+    console.log(i,j)
 }
+
+// ランダムな3桁の配列
+// String(Math.floor(Math.random() * 1000)).split('')
+
+// 重複のないランダムな文字列
